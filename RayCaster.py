e4f00f03b7b4ff379b152cf6ee7d93ddaeecef8a
coutino568 <40869460+coutino568@users.noreply.github.com>
2021-11-17 18:40:43 -0600

    garden

--- RayCaster.py
+++ RayCaster.py
@@ -2,6 +2,8 @@ import pygame
 import pygame_menu
 
 from math import cos, sin, pi
+from pygame import mixer
+
 
 RAY_AMOUNT = 100
 
@@ -14,8 +16,12 @@ RAY_AMOUNT = 100
 #     }
 
 wallTextures = {
-    '1': pygame.image.load('wall1.png')
+    '1': pygame.image.load('wall1.png'),
+    '2': pygame.image.load('wall2.png'),
+    '3': pygame.image.load('wall3.png')
+
     }
+
 
 
 class Raycaster(object):
@@ -37,7 +43,8 @@ class Raycaster(object):
            'x' : 100,
            'y' : 175,
            'fov': 60,
-           'angle': 180 }
+           'angle': 0 ,
+           'pitch': 0}
 
 
     def load_map(self, filename):
@@ -126,12 +133,16 @@ class Raycaster(object):
             angle = self.player['angle'] - (self.player['fov'] / 2) + (self.player['fov'] * column / RAY_AMOUNT)
             dist, id, tx = self.castRay(angle)
 
+            #rayWidth = 0
             rayWidth = int(( 1 / RAY_AMOUNT) * halfWidth)
 
             startX = halfWidth + int(( (column / RAY_AMOUNT) * halfWidth))
 
             # perceivedHeight = screenHeight / (distance * cos( rayAngle - viewAngle)) * wallHeight
             h = self.height / (dist * cos( (angle - self.player["angle"]) * pi / 180)) * self.wallheight
+            
+            #startY =int(0)
+            #endY = int (500)
             startY = int(halfHeight - h/2)
             endY = int(halfHeight + h/2)
 
@@ -172,14 +183,14 @@ def start_the_game():
    
 
 
-width = 1000
+width = 1000 
 height = 500
 
 pygame.init()
 screen = pygame.display.set_mode((width,height), pygame.DOUBLEBUF | pygame.HWACCEL )
 screen.set_alpha(None)
 
-menu = pygame_menu.Menu('MAIN MENU', 1000, 500,
+menu = pygame_menu.Menu('MAIN MENU', width, height,
                        theme=pygame_menu.themes.THEME_DARK)
 
 menu.add.selector('Level :', [('1', 1), ('2', 2)], onchange=set_difficulty)
@@ -188,6 +199,9 @@ menu.add.button('Quit', pygame_menu.events.EXIT)
 
 isRunning = True
 isPaused= False
+
+mixer.music.load('music.mp3')
+mixer.music.play(-1)
 
 rCaster = Raycaster(screen)
 rCaster.load_map(map)
@@ -216,54 +230,140 @@ def updateStatus():
     stat = font.render(mode, 1, pygame.Color("yellow")) 
     return stat
 
+m_forward=False
+m_backward=False
+m_left= False
+m_right= False
+t_right=False
+t_left=False
 
 while isRunning:
+    newX = rCaster.player['x']
+    newY = rCaster.player['y']
+    forward = rCaster.player['angle'] * pi / 180
+    right = (rCaster.player['angle'] + 90) * pi / 180
+    #forward = rCaster.player['angle'] * pi / 180
+    #right = (rCaster.player['angle'] + 90) * pi / 180
+
+
+    if m_forward==True and isPaused==False :
+        newX += cos(forward) * rCaster.stepSize
+        newY += sin(forward) * rCaster.stepSize
+        # i = int(newX/rCaster.blocksize)
+        # j = int(newY/rCaster.blocksize)
+
+        # if rCaster.map[j][i] == ' ':
+        #     rCaster.player['x'] = newX
+        #     rCaster.player['y'] = newY
+
+    if m_backward==True and isPaused==False :
+        
+        newX -= cos(forward) * rCaster.stepSize
+        newY -= sin(forward) * rCaster.stepSize
+        # i = int(newX/rCaster.blocksize)
+        # j = int(newY/rCaster.blocksize)
+
+        # if rCaster.map[j][i] == ' ':
+        #     rCaster.player['x'] = newX
+        #     rCaster.player['y'] = newY
+
+    if m_right==True and isPaused==False:
+        
+        newX += cos(right) * rCaster.stepSize
+        newY += sin(right) * rCaster.stepSize
+        # i = int(newX/rCaster.blocksize)
+        # j = int(newY/rCaster.blocksize)
+
+        # if rCaster.map[j][i] == ' ':
+        #     rCaster.player['x'] = newX
+        #     rCaster.player['y'] = newY
+    if m_left==True and isPaused==False :
+        
+        newX -= cos(right) * rCaster.stepSize
+        newY -= sin(right) * rCaster.stepSize
+        # i = int(newX/rCaster.blocksize)
+        # j = int(newY/rCaster.blocksize)
+
+        # if rCaster.map[j][i] == ' ':
+        #     rCaster.player['x'] = newX
+        #     rCaster.player['y'] = newY
+
+
+    if t_left and isPaused==False:
+        rCaster.player['angle'] -= rCaster.turnSize
+
+    if t_right and isPaused==False:
+        rCaster.player['angle'] += rCaster.turnSize
+
+    i = int(newX/rCaster.blocksize)
+    j = int(newY/rCaster.blocksize)
+
+    if rCaster.map[j][i] == ' ':
+        rCaster.player['x'] = newX
+        rCaster.player['y'] = newY
+
 
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             isRunning = False
 
         elif ev.type == pygame.KEYDOWN:
-            newX = rCaster.player['x']
-            newY = rCaster.player['y']
-            forward = rCaster.player['angle'] * pi / 180
-            right = (rCaster.player['angle'] + 90) * pi / 180
 
             if ev.key == pygame.K_ESCAPE:
                 isPaused = not isPaused
             elif isPaused==False:
+                #navigation forward
                 if ev.key == pygame.K_w:
-                    newX += cos(forward) * rCaster.stepSize
-                    newY += sin(forward) * rCaster.stepSize
-                elif ev.key == pygame.K_s:
-                    newX -= cos(forward) * rCaster.stepSize
-                    newY -= sin(forward) * rCaster.stepSize
-                elif ev.key == pygame.K_a:
-                    newX -= cos(right) * rCaster.stepSize
-                    newY -= sin(right) * rCaster.stepSize
-                elif ev.key == pygame.K_d:
-                    newX += cos(right) * rCaster.stepSize
-                    newY += sin(right) * rCaster.stepSize
-                elif ev.key == pygame.K_q:
-                    rCaster.player['angle'] -= rCaster.turnSize
-                elif ev.key == pygame.K_e:
-                    rCaster.player['angle'] += rCaster.turnSize
+                    m_forward=True
+                else:
+                    m_forward=False
+                #NANAvigation left
+                if ev.key == pygame.K_s:
+                    m_backward=True
+                else:
+                    m_backward=False
+                #navigation left
+                if ev.key == pygame.K_a:
+                    m_left = True
+                    
+                else:
+                    m_left=False
+                #navigation right
+                if ev.key == pygame.K_d:
+                    m_right = True
+                    
+                else:
+                    m_right=False
+                if ev.key == pygame.K_q:
+                    t_left=True
+                else:
+                    t_left=False
+                if ev.key == pygame.K_e:
+                    t_right=True
+                else:
+                    t_right=False
+        elif ev.type == pygame.KEYUP:
+            #print("Released")
+            m_forward=False
+            m_backward=False
+            m_left= False
+            m_right= False
+            t_right=False
+            t_left=False
+                    
 
-            i = int(newX/rCaster.blocksize)
-            j = int(newY/rCaster.blocksize)
-
-            if rCaster.map[j][i] == ' ':
-                rCaster.player['x'] = newX
-                rCaster.player['y'] = newY
+            #226324
+            #4B7238
 
 
-    screen.fill(pygame.Color("gray"))
+
+    screen.fill(pygame.Color(75,110,50,255))
 
     # Techo
-    screen.fill(pygame.Color("saddlebrown"), (int(width / 2), 0,  int(width / 2), int(height / 2)))
+    screen.fill(pygame.Color(115,240,245,255), (int(width / 2), 0,  int(width / 2), int(height / 2)))
 
     # Piso
-    screen.fill(pygame.Color("dimgray"), (int(width / 2), int(height / 2),  int(width / 2), int(height / 2)))
+    screen.fill(pygame.Color(75,110,50,255), (int(width / 2), int(height / 2),  int(width / 2), int(height / 2)))
 
 
     rCaster.render()
@@ -273,7 +373,7 @@ while isRunning:
     screen.blit(updateFPS(), (0,0))
 
     screen.blit(updateStatus(),(50,0))
-    clock.tick(60)
+    clock.tick(120)
 
 
     pygame.display.flip()
